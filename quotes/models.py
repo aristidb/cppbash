2e8cfdc7fae6506c287f9155058642ff5fa93fa4
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2010-04-03 20:36:26 +0200

    datetime

--- quotes/models.py
+++ quotes/models.py
@@ -2,7 +2,7 @@ from appengine_django.models import BaseModel
 from google.appengine.ext import db
 
 class Quote(BaseModel):
-    creation_date=db.DateProperty()
+    creation_date=db.DateTimeProperty()
     language=db.StringProperty()
     quote=db.StringProperty()
 
