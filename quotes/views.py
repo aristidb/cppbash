2127dc8ae7b044187bfee8c17bf95a3539d145ff
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2010-04-04 01:17:07 +0200

    display only accepted quotes

--- quotes/views.py
+++ quotes/views.py
@@ -3,5 +3,5 @@ from google.appengine.ext import db
 from quotes import models
 
 def home(request):
-    q = models.Quote.all().order('creation_date')
+    q = models.Quote.all().filter('accepted =', True).order('creation_date')
     return render_to_response('quotes/index.html', { 'quotes': q })
