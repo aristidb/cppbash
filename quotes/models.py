6aa8510e738530301da355b108ead2a51ce75757
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2010-04-04 00:38:02 +0200

    expand model

--- quotes/models.py
+++ quotes/models.py
@@ -3,6 +3,9 @@ from google.appengine.ext import db
 
 class Quote(BaseModel):
     creation_date=db.DateTimeProperty()
-    language=db.StringProperty()
+    language=db.CategoryProperty()
     quote=db.TextProperty()
+    accepted=db.BooleanProperty()
+    submitter_ip=db.StringProperty()
+    submitter_email=db.EmailProperty()
 
