0841081b9701e2556c125d49e89793b3ea953381
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2010-04-04 02:33:23 +0200

    add reviewer concept

--- quotes/models.py
+++ quotes/models.py
@@ -1,11 +1,15 @@
 from appengine_django.models import BaseModel
 from google.appengine.ext import db
 
+class Reviewer(BaseModel):
+    user=db.UserProperty()
+
 class Quote(BaseModel):
     creation_date=db.DateTimeProperty()
     language=db.CategoryProperty()
     quote=db.TextProperty()
     accepted=db.BooleanProperty()
+    accepted_by=db.ReferenceProperty(Reviewer)
+    accepted_date=db.DateTimeProperty()
     submitter_ip=db.StringProperty()
     submitter_email=db.EmailProperty()
-
