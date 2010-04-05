23808564371e64dd2e783bb9cc3b1b20bd4138a1
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2010-04-05 21:24:53 +0200

    json

--- home.py
+++ home.py
@@ -1,7 +1,9 @@
 from tipfy import RequestHandler, request, Response
 from tipfy.ext.jinja2 import render_template
+from tipfy.ext.db import get_property_dict
 from google.appengine.ext import db
 from google.appengine.api import users
+from django.utils import simplejson
 import models
 
 def _filter(name, request, response):
@@ -19,9 +21,27 @@ def _alternatives(removed, standard):
         possible.remove(removed)
     return possible
 
+def _json(qs):
+    r = []
+    for q in qs:
+        d = get_property_dict(q)
+        r.append({
+                'creation_date': str(d['creation_date']),
+                'language': str(d['language']),
+                'programming_language': str(d['programming_language']),
+                'quote': unicode(d['quote']),
+                })
+    return r
+
 class HomeHandler(RequestHandler):
     def get(self, **kwargs):
-        response = Response(mimetype = 'text/html')
+        json = request.is_xhr or request.args.get('json', '')
+
+        if json:
+            response = Response(mimetype = 'application/json')
+        else:
+            response = Response(mimetype = 'text/html')
+
         language = _filter('language', request, response)
         programming_language = _filter('programming_language', request, response)
         
@@ -36,13 +56,16 @@ class HomeHandler(RequestHandler):
         languages = _alternatives(language, models.languages)
         programming_languages = _alternatives(programming_language, models.programming_languages)
 
-        out = render_template(
-            'index.html',
-            quotes = q,
-            language = language,
-            languages = languages,
-            programming_language = programming_language,
-            programming_languages = programming_languages)
+        if json:
+            out = simplejson.dumps(_json(q))
+        else:
+            out = render_template(
+                'index.html',
+                quotes = q,
+                language = language,
+                languages = languages,
+                programming_language = programming_language,
+                programming_languages = programming_languages)
 
         response.response = [out]
 
